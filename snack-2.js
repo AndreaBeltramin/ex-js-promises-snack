b01f3945ecc9e086934d90e2c1da495cb29532fa
AndreaBeltramin <andreabeltra99@gmail.com>
2025-02-26 12:27:12 +0100

    snack 2

--- snack-2.js
+++ snack-2.js
@@ -1,6 +1,24 @@
-// 🏆 Snack 2
+// Snack 2
 // Crea la funzione lanciaDado() che restituisce una Promise che, dopo 3 secondi,
 // genera un numero casuale tra 1 e 6. Tuttavia, nel 20% dei casi, il dado si "incastra" e la Promise va in reject.
+
+function lanciaDado() {
+	return new Promise((resolve, reject) => {
+		console.log("lancio del dado...");
+		setTimeout(() => {
+			if (Math.random() < 0.2) {
+				reject("dado incastrato...riprova");
+			} else {
+				const risultato = Math.floor(Math.random() * 6) + 1;
+				resolve(risultato);
+			}
+		}, 3000);
+	});
+}
+
+lanciaDado()
+	.then((risultato) => console.log("numero : ", risultato))
+	.catch((err) => console.error(err));
 
 // Bonus: HOF con closure per memorizzare l'ultimo lancio
 // Modifica la funzione in creaLanciaDado(), che restituisce una closure che memorizza
